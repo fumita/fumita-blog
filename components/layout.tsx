658db05cb7b4a8119197da255b4abcb0557b2604
fumita <maikiilion@gmail.com>
2021-03-15 00:15:19 +0900

    ok

--- components/layout.tsx
+++ components/layout.tsx
@@ -36,30 +36,10 @@ const Layout: React.FC<Props> = (props: Props) => {
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
-              src="/images/profile.webp"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
             <h1 className={utilStyles.heading2Xl}>{name}</h1>
           </>
         ) : (
           <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.webp"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
             <h2 className={utilStyles.headingLg}>
               <Link href="/">
                 <a className={utilStyles.colorInherit}>{name}</a>
